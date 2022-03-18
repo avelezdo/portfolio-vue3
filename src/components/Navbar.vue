4986df76fcad29b14e91fc702e25ca3841f30a7b
avelezdo <albertovelezdev@gmail.com>
2022-03-18 20:27:32 +0100

    Se corrige estilo en el botón de 'CV'

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,8 +16,6 @@
 							alt="Download file icon"
 					/></a>
 				</li>
-				<!-- <li class="nav__link" data-name="recommendations">Testimonials</li> -->
-				<!-- <li class="nav__link" data-name="contact">Contacto</li> -->
 			</ul>
 		</div>
 		<div id="burger">
@@ -29,176 +27,182 @@
 </template>
 
 <script>
-export default {
-	name: 'Navbar',
-	mounted: function () {
-		const burger = document.getElementById('burger'),
-			navContents = document.querySelector('.mobile-nav__container'),
-			navItems = document.querySelectorAll('.nav__link');
+	export default {
+		name: 'Navbar',
+		mounted: function () {
+			const burger = document.getElementById('burger'),
+				navContents = document.querySelector('.mobile-nav__container'),
+				navItems = document.querySelectorAll('.nav__link')
 
-		burger.addEventListener('click', function () {
-			navContents.classList.toggle('toggle-nav');
-		});
+			burger.addEventListener('click', function () {
+				navContents.classList.toggle('toggle-nav')
+			})
 
-		navContents.addEventListener('transitionend', function () {
-			if (navContents.classList.contains('toggle-nav')) {
-				navItems.forEach((item, index) => {
-					item.classList.add(`appear-${index + 1}`);
-				});
-			} else {
-				navItems.forEach((item, index) => {
-					item.classList.remove(`appear-${index + 1}`);
-				});
-			}
-		});
+			navContents.addEventListener('transitionend', function () {
+				if (navContents.classList.contains('toggle-nav')) {
+					navItems.forEach((item, index) => {
+						item.classList.add(`appear-${index + 1}`)
+					})
+				} else {
+					navItems.forEach((item, index) => {
+						item.classList.remove(`appear-${index + 1}`)
+					})
+				}
+			})
 
-		navItems.forEach((item) => {
-			item.addEventListener('click', function () {
-				navContents.classList.toggle('toggle-nav');
-			});
-		});
-	},
-};
+			navItems.forEach((item) => {
+				item.addEventListener('click', function () {
+					navContents.classList.toggle('toggle-nav')
+				})
+			})
+		},
+	}
 </script>
 
 <style lang="scss" scoped>
-nav.navbar {
-	@include flex(row, space-around, center);
-	z-index: 1;
-	background: $blue__dark;
-	color: $white;
-	padding: 1rem;
-	box-shadow: 2px 2px 12px 2px rgba(0, 0, 0, 0.1);
-	margin-top: 10px;
-	position: relative;
-	@include nav-collapse {
-		margin-top: 0px;
-		@include flex(row, space-between, center);
-		padding-left: 2rem;
-		padding-right: 2rem;
-		position: fixed;
-		top: 0;
-		left: 0;
-		right: 0;
-	}
-	.nav__header {
-		font-family: 'Fira Sans', arial, sans-serif;
-		font-size: 1.8rem;
-		letter-spacing: 1px;
-		cursor: pointer;
-		@include mobile {
-			font-size: 1.5rem;
-		}
-	}
-	.mobile-nav__container {
+	nav.navbar {
+		@include flex(row, space-around, center);
+		z-index: 1;
 		background: $blue__dark;
-		z-index: -1;
-		@include flex(row, space-between, center);
+		color: $white;
+		padding: 1rem;
+		box-shadow: 2px 2px 12px 2px rgba(0, 0, 0, 0.1);
+		margin-top: 10px;
+		position: relative;
 		@include nav-collapse {
-			margin-top: 50px;
+			margin-top: 0px;
+			@include flex(row, space-between, center);
+			padding-left: 2rem;
+			padding-right: 2rem;
 			position: fixed;
-			right: 0;
 			top: 0;
-			transform: translateY(-200%);
-			width: 100%;
-			transition: all 0.25s;
-			z-index: -99;
+			left: 0;
+			right: 0;
 		}
-	}
-	.nav__ul {
-		@include flex(row, center, center);
-		list-style: none;
-		@include nav-collapse {
-			@include flex(column, space-evenly, flex-start);
-			padding: 2rem 0;
-		}
-		@include mobile {
-			@include flex(column, space-evenly, flex-start);
-		}
-		.nav__link {
-			padding: 8px 12px;
+		.nav__header {
+			font-family: 'Fira Sans', arial, sans-serif;
+			font-size: 1.8rem;
+			letter-spacing: 1px;
 			cursor: pointer;
-			transition: all 0.25s;
-			@include nav-collapse {
-				transform: translateX(-500%);
-				font-size: 1.4rem;
-				padding-left: 2rem;
-				margin-top: 1rem;
-				margin-bottom: 1rem;
-				margin: 5px;
-				text-align: center;
+			@include mobile {
+				font-size: 1.5rem;
 			}
-			&:hover {
-				background: $white;
-				color: $blue__dark;
-				img {
-					filter: brightness(0);
+		}
+		.mobile-nav__container {
+			background: $blue__dark;
+			z-index: -1;
+			@include flex(row, space-between, center);
+			@include nav-collapse {
+				margin-top: 50px;
+				position: fixed;
+				right: 0;
+				top: 0;
+				transform: translateY(-200%);
+				width: 100%;
+				transition: all 0.25s;
+				z-index: -99;
+			}
+		}
+		.nav__ul {
+			@include flex(row, center, center);
+			list-style: none;
+			@include nav-collapse {
+				@include flex(column, space-evenly, flex-start);
+				padding: 2rem 0;
+			}
+			@include mobile {
+				@include flex(column, space-evenly, flex-start);
+			}
+			.nav__link {
+				padding: 8px 12px;
+				cursor: pointer;
+				transition: all 0.25s;
+				a {
+					color: $white;
+				}
+				@include nav-collapse {
+					transform: translateX(-500%);
+					font-size: 1.4rem;
+					padding-left: 2rem;
+					margin-top: 1rem;
+					margin-bottom: 1rem;
+					margin: 5px;
+					text-align: center;
+				}
+				&:hover {
+					background: $white;
+					color: $blue__dark;
+					img {
+						filter: brightness(0);
+					}
+					a {
+						color: $blue__dark;
+					}
+				}
+			}
+			.CV img {
+				height: 20px;
+				margin-bottom: -3px;
+				filter: brightness(10);
+			}
+			@for $i from 1 through 6 {
+				.nav__link.appear-#{$i} {
+					animation: mobile-navigation 500ms ease-out 60ms 1 forwards;
 				}
 			}
 		}
-		.CV img {
-			height: 20px;
-			margin-bottom: -3px;
-			filter: brightness(10);
-		}
-		@for $i from 1 through 6 {
-			.nav__link.appear-#{$i} {
-				animation: mobile-navigation 500ms ease-out 60ms 1 forwards;
+		#burger {
+			display: none;
+			width: 32px;
+			height: 32px;
+			cursor: pointer;
+			@include nav-collapse {
+				display: inline-block;
+			}
+			.line {
+				width: 100%;
+				height: 2px;
+				background: white;
+				margin-top: 7px;
+				margin-bottom: 7px;
+				border-radius: 5px;
 			}
 		}
-	}
-	#burger {
-		display: none;
-		width: 32px;
-		height: 32px;
-		cursor: pointer;
-		@include nav-collapse {
-			display: inline-block;
-		}
-		.line {
-			width: 100%;
-			height: 2px;
-			background: white;
-			margin-top: 7px;
-			margin-bottom: 7px;
-			border-radius: 5px;
+		.toggle-nav {
+			transform: translateY(0%);
+			transition: all 0.15s;
 		}
 	}
-	.toggle-nav {
-		transform: translateY(0%);
-		transition: all 0.15s;
-	}
-}
 
-@keyframes mobile-navigation {
-	0% {
-		transform: translateX(-500%);
-		opacity: 0;
+	@keyframes mobile-navigation {
+		0% {
+			transform: translateX(-500%);
+			opacity: 0;
+		}
+		30% {
+			transform: translateX(50%);
+		}
+		60% {
+			transform: translateX(0%);
+			opacity: 1;
+		}
+		80% {
+			transform: translateX(8%);
+		}
+		100% {
+			transform: translateX(0%);
+			opacity: 1;
+		}
 	}
-	30% {
-		transform: translateX(50%);
-	}
-	60% {
-		transform: translateX(0%);
-		opacity: 1;
-	}
-	80% {
-		transform: translateX(8%);
-	}
-	100% {
-		transform: translateX(0%);
-		opacity: 1;
-	}
-}
 
-@keyframes close-mobile-navigation {
-	0% {
-		transform: translateY(0%);
-		opacity: 1;
+	@keyframes close-mobile-navigation {
+		0% {
+			transform: translateY(0%);
+			opacity: 1;
+		}
+		100% {
+			transform: translateY(100%);
+			opacity: 0;
+		}
 	}
-	100% {
-		transform: translateY(100%);
-		opacity: 0;
-	}
-}
 </style>
